5a1581cf6dfd74fc4261ee5e550077d3f5883d67
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2026-03-03 09:59:23 +0100

    Commit

---
+++ src/components/ComposantCalculatrice.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import BtnCalc from "./BtnCalc.vue";
+const nombre = ref(0);
+const modeDecimal = ref(false);
+const rang = ref(0);
+</script>
+
+<template>
+  <h1>Calculatrice</h1>
+  <p>Affichage : {{ nombre }}</p>
+
+  <div class="grid grid-cols-3 grid-rows-3">
+    <BtnCalc
+      v-for="chiffre in 9"
+      v-model="nombre"
+      :chiffre="chiffre"
+      :modeDecimal="modeDecimal"
+      :rang="rang"
+    />
+    <button @click="nombre = 0">Clear</button>
+    <BtnCalc
+      v-model="nombre"
+      :chiffre="0"
+      :modeDecimal="modeDecimal"
+      :rang="rang"
+    />
+    <button>.</button>
+    <button @click="nombre = Math.floor(nombre / 10)"><=</button>
+  </div>
+
+  <!-- <button @click="increment()">Incrémenter</button>
+  <button @click="decrement()">Décrementer</button> -->
+</template>
--- src/components/SelectPersonnes.vue
+++ src/components/SelectPersonnes.vue
@@ -1,20 +1,48 @@
 <script setup lang="ts">
-import type Multiselect from "@vueform/multiselect";
+import Multiselect from "@vueform/multiselect";
 import "@vueform/multiselect/themes/default.css";
 import type { PersonnesResponse } from "../pocketbase-types";
 import { ref } from "vue";
 const props = defineProps<{
   personnes: PersonnesResponse[];
+  name: string;
+  value: string[] | string;
+  multiple?: boolean;
+  placeholder?: string;
 }>();
-const personneId = ref();
+const personneListId = ref(props.value);
 console.log("Ici select", props.personnes);
 </script>
 
 <template>
   <Multiselect
-    v-model="personneId"
+    v-model="personneListId"
     :options="personnes"
+    searchable
+    :mode="multiple ? 'tags' : 'single'"
     label="nom"
     value-prop="id"
+    :placeholder="placeholder"
   />
+  valeur: {{ personneListId }}
+  <!-- @vue-ignore -->
+  <input
+    v-for="p in multiple ? personneListId : [personneListId]"
+    type="hidden"
+    :name="name"
+    :value="p"
+    :key="p"
+  />
+
+  <!-- <input
+    v-if="multiple"
+    v-for="p in personneListId"
+    type="hidden"
+    :name="name"
+    :value="p"
+    :key="p"
+  />
+  <input v-else type="hidden" :name="name" :value="personneListId" /> -->
 </template>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
